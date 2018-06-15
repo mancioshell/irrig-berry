84139bc18ff3992c270e30e15be80195159ae647
mancioshell <mancioshell@localhost.localdomain>
2018-06-15 19:53:10 +0200

    Enable gpio

--- waterberry/gpio/board.py
+++ waterberry/gpio/board.py
@@ -1,4 +1,4 @@
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 from waterberry.utils.logger import logger
 
@@ -8,31 +8,30 @@ class Board:
 
     def initBoard(self):
         logger.debug('int bread board ...')
-        #GPIO.setwarnings(False)
-        #GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
 
     def setupOutputPin(self, pin):
         logger.debug('setupOutputPin ... {}'.format(pin))
-        #GPIO.setup(pin, GPIO.OUT)
+        GPIO.setup(pin, GPIO.OUT)
 
     def setupInputPin(self, pin):
         logger.info('setupInputPin ... {}'.format(pin))
-        #GPIO.setup(pin, GPIO.IN)
+        GPIO.setup(pin, GPIO.IN)
 
     def enablePin(self, pin):
         logger.info('enablePin ... {}'.format(pin))
-        #GPIO.output(pin, GPIO.LOW) # Pin on
+        GPIO.output(pin, GPIO.LOW) # Pin on
 
     def disablePin(self, pin):
         logger.info('disablePin ... {}'.format(pin))
-        #GPIO.output(pin, GPIO.HIGH) # Pin on
+        GPIO.output(pin, GPIO.HIGH) # Pin on
 
     def getPinState(self, pin):
-        #state = GPIO.input(pin)
-        state = 2
+        state = GPIO.input(pin)
         logger.info('getPinState ... {}'.format(state))
         return state
 
     def cleanupPin(self, pin):
         logger.info('cleanupPin ... {}'.format(pin))
-        #GPIO.cleanup(pin)
+        GPIO.cleanup(pin)
